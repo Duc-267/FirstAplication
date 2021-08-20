c2452afe0dccf0c58323ec759357100313bafcb7
Duc-267 <duc2672002@gmail.com>
2021-08-20 22:25:30 +0700

    add fragment

--- app/src/main/java/com/example/myapplication/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -1,11 +1,39 @@
 package com.example.myapplication
 
+import FirstFragment
+import SecondFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, firstFragment)
+            addToBackStack(null)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btnFragment2.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
